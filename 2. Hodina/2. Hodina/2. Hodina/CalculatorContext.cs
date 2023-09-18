6904fc65c5c44bc9cf7ceedaa24f449096a44b2c
venda611 <kroutil.vaclav@post.cz>
2023-09-18 14:09:43 +0200

    Added: calculator

---
+++ 2. Hodina/2. Hodina/2. Hodina/CalculatorContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._Hodina
+{
+    internal class CalculatorContext
+    {
+        private IOperation operation;
+
+        public CalculatorContext(IOperation operation) 
+        {
+            this.operation = operation;
+        }
+        
+        public int executeOperation(int a, int b) {
+            return this.operation.execute(a, b);
+        }
+    
+    }
+}
---
+++ 2. Hodina/2. Hodina/2. Hodina/Program.cs
@@ -0,0 +1,15 @@
+﻿using _2._Hodina.operations;
+
+namespace _2._Hodina
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            CalculatorContext context = new CalculatorContext(new OperationAdd());
+
+            Console.WriteLine($"4 + 5 = " + context.executeOperation(4,5);
+        }
+    }
+}
